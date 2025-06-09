ff44d1b2e2e4450aa6f60b37b1c5a0be6225e03f
andrea21gonzarza <andrea21gonzarza@gmail.com>
2025-06-08 18:30:55 -0600

    Modificación del tamaño del número de la pregunta

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -12,69 +12,68 @@ class QuestionsSummary extends StatelessWidget {
       height: 400,
       child: SingleChildScrollView(
         child: Column(
-          children: summaryData.map((data) {
-            return Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(right: 15),
-                  width: 30,
-                  height: 30,
-                  padding: const EdgeInsets.only(top: 4),
-                  decoration: BoxDecoration(
-                      color: data['user_answer'] == data['correct_answer']
-                          ? const Color.fromARGB(255, 64, 164, 231)
-                          : const Color.fromARGB(255, 242, 124, 247),
-                      borderRadius: BorderRadius.circular(17)),
-                  child: Text(
-                    ((data['question_index'] as int) + 1).toString(),
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
+          children:
+              summaryData.map((data) {
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(right: 15),
+                      width: 30,
+                      height: 30,
+                      padding: const EdgeInsets.only(top: 4),
+                      decoration: BoxDecoration(
+                        color:
+                            data['user_answer'] == data['correct_answer']
+                                ? const Color.fromARGB(255, 64, 164, 231)
+                                : const Color.fromARGB(255, 242, 124, 247),
+                        borderRadius: BorderRadius.circular(17),
+                      ),
+                      child: Text(
+                        ((data['question_index'] as int) + 1).toString(),
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        data['question'] as String,
-                        style: GoogleFonts.lato(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 16,
-                        ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data['question'] as String,
+                            style: GoogleFonts.montserrat(
+                              color: const Color.fromARGB(255, 255, 255, 255),
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            data['user_answer'] as String,
+                            style: GoogleFonts.montserrat(
+                              color: const Color.fromARGB(255, 208, 147, 248),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            data['correct_answer'] as String,
+                            style: GoogleFonts.montserrat(
+                              color: const Color.fromARGB(255, 147, 182, 248),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
                       ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        data['user_answer'] as String,
-                        style: GoogleFonts.lato(
-                          color: const Color.fromARGB(255, 208, 147, 248),
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        data['correct_answer'] as String,
-                        style: GoogleFonts.lato(
-                          color: const Color.fromARGB(255, 147, 182, 248),
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            );
-          }).toList(),
+                    ),
+                  ],
+                );
+              }).toList(),
         ),
       ),
     );
